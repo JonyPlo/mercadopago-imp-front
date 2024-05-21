0ea72f8591a1281f5093e52f34a2221d6691c7dd
JonyPlo <joonyyplo@gmail.com>
2024-05-21 00:23:37 -0300

    feat: add mercadopago example implementation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Product } from './components/Product'
+
+function App() {
+  return (
+    <section className='min-h-screen w-full flex flex-col items-center justify-center bg-slate-900'>
+      <Product />
+    </section>
+  )
+}
+
+export default App
